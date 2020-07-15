90f4f5cd0bd356be850345b4f86ad9f88c7c99f4
ChanduReddy154 <chandureddy938@gmail.com>
2020-07-15 16:20:11 +0530

    Initial Commit

---
+++ DataBasePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataBasePractice
+//
+//  Created by Chandu Reddy on 15/07/20.
+//  Copyright © 2020 Chandu Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
